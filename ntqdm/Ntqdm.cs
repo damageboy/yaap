7f8fd6513b9b1dc937a3acc651c20f33c02291ea
Dan Shechter <dans@houmus.org>
2018-04-23 18:57:11 +0300

    Initial something to push

---
+++ ntqdm/Ntqdm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ntqdm
+{
+    public class Ntqdm : IDisposable
+    {
+      static object _consoleLock = new object();
+      static ConcurrentDictionary<Ntqdm, bool> _instances = new ConcurrentDictionary<Ntqdm, bool>();
+
+      static void AddInstance(Ntqdm ntqdm) => _instances.TryAdd(ntqdm, true);
+      static void RemoveInstance(Ntqdm ntqdm) => _instances.TryRemove(ntqdm, out var _);
+
+      public Ntqdm()
+      {
+        AddInstance(this);
+      }
+
+
+      public void Dispose()
+      {
+        RemoveInstance(this);
+      }
+    }
+}
