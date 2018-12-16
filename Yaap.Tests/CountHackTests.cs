0d0b6e105d2926151a3a4084740dd3232dc6321b
Dan Shechter <dans@houmus.org>
2018-12-16 21:51:02 +0200

    [Tests] make tests static

--- Yaap.Tests/CountHackTests.cs
+++ Yaap.Tests/CountHackTests.cs
@@ -7,7 +7,7 @@ namespace Yaap.Tests
     public class CountHackTests
     {
         [Test]
-        public void IIListProviderExists()
+        public static void IIListProviderExists()
         {
             //var iilp = typeof(Enumerable).Assembly.GetTypes().Single(t => t.Name.Contains("IIListProvider"));
 
@@ -26,42 +26,42 @@ namespace Yaap.Tests
 
 
         [Test]
-        public void EnumerableRangeWorks()
+        public static void EnumerableRangeWorks()
         {
             var range = Enumerable.Range(0, 100);
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(100));
         }
 
         [Test]
-        public void ConcatWorks()
+        public static void ConcatWorks()
         {
             var range = Enumerable.Range(0, 100).Concat(Enumerable.Range(100, 100));
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(200));
         }
 
         [Test]
-        public void SelectWorks()
+        public static void SelectWorks()
         {
             var range = Enumerable.Range(0, 100).Select(x => x * x);
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(100));
         }
 
         [Test]
-        public void ReverseWorks()
+        public static void ReverseWorks()
         {
             var range = Enumerable.Range(0, 100).Reverse();
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(100));
         }
 
         [Test]
-        public void OrderByWorks()
+        public static void OrderByWorks()
         {
             var range = Enumerable.Range(0, 100).OrderByDescending(x => x);
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(100));
         }
 
         [Test]
-        public void AppendWorks()
+        public static void AppendWorks()
         {
             var range = Enumerable.Range(0, 100).Append(101);
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(101));
@@ -69,28 +69,28 @@ namespace Yaap.Tests
 
 
         [Test]
-        public void PrependWorks()
+        public static void PrependWorks()
         {
             var range = Enumerable.Range(0, 100).Prepend(-1);
             Assert.That(YaapEnumerable<int>.GetCheapCount(range), Is.EqualTo(101));
         }
 
         [Test]
-        public void ListWorks()
+        public static void ListWorks()
         {
             var list = Enumerable.Range(0, 100).ToList();
             Assert.That(YaapEnumerable<int>.GetCheapCount(list), Is.EqualTo(100));
         }
 
         [Test]
-        public void ArrayWorks()
+        public static void ArrayWorks()
         {
             var array = Enumerable.Range(0, 100).ToArray();
             Assert.That(YaapEnumerable<int>.GetCheapCount(array), Is.EqualTo(100));
         }
 
         [Test]
-        public void DictionaryWorks()
+        public static void DictionaryWorks()
         {
             var dict = Enumerable.Range(0, 100).ToDictionary(x => x, x => x);
             Assert.That(YaapEnumerable<KeyValuePair<int, int>>.GetCheapCount(dict), Is.EqualTo(100));
